281dc83874cd6b98dc56b2c1782e3729967b1b63
Francesco Pandolfi <francesco.pan@gmail.com>
2020-01-17 16:19:20 +0100

    Frecce destra sinistra
    Aggiunte azioni su frecce avanti e indietro

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
+
+
 $( document ).ready(function() {
-
-
-  $('.next').click(function () {
-
-      $('.images img.active').removeClass('active');
-      $('.images img.active').next().addClass('active');
-
-    });
-
-
-
-
+  // Next arrow actions
+  $('.next').click(function () { showNext(); });
+  // Prev arrow actions
+  $('.prev').click(function () { showPrev(); });
 
 });
+
+function showNext() {
+  var imageActive = $('.images img.active');
+  var imageNext = $('.images img.active').next('img');
+  var imageLast = $('.images img:last-child');
+  var imageFirst = $('.images img:first-child');
+
+
+  imageActive.removeClass('active');
+  imageNext.addClass('active');
+
+  if (imageLast.hasClass('active')) {
+    imageFirst.addClass('active')
+  }
+}
+
+function showPrev() {
+  var imageActive = $('.images img.active');
+  var imagePrev = $('.images img.active').prev('img');
+  var imageLast = $('.images img:last-child');
+  var imageFirst = $('.images img:first-child');
+
+  imageActive.removeClass('active');
+  imagePrev.addClass('active');
+
+  if (imageFirst.hasClass('active')) {
+    imageLast.addClass('active')
+  }
+}
